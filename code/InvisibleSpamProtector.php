539523dd64f27ad469ab7058e7d5c1542cfc9a2e
David Hargitai <dhargitai@sessiondigital.com>
2014-04-30 17:02:03 +0200

    Fix style issues

--- code/InvisibleSpamProtector.php
+++ code/InvisibleSpamProtector.php
@@ -4,23 +4,30 @@
  * @package InvisibleSpamProtection
  */
 
-class InvisibleSpamProtector implements SpamProtector {
-	/**
-	 * Returns the {@link InvisibleSpamProtectorField} associated with this protector
-	 *
-	 * @return InvisibleSpamProtectorField
-	 */
-	public function getFormField($name = null, $title = null, $value = null, $form = null, $rightTitle = null) {
-		return new InvisibleSpamProtectorField($name, $title, $value, $form, $rightTitle);
-	}
+class InvisibleSpamProtector implements SpamProtector
+{
+    /**
+     * Returns the {@link InvisibleSpamProtectorField} associated with this protector
+     *
+     * @return InvisibleSpamProtectorField
+     */
+    public function getFormField($name = null, $title = null, $value = null, $form = null, $rightTitle = null)
+    {
+        return new InvisibleSpamProtectorField($name, $title, $value, $form, $rightTitle);
+    }
 
-	/**
-	 * Function required to handle dynamic feedback of the system.
-	 * if unneeded just return true
-	 *
-	 * @return true
-	 */
-	public function sendFeedback($object = null, $feedback = "") {
-		return true;
-	}
+    /**
+     * Function required to handle dynamic feedback of the system.
+     * if unneeded just return true
+     *
+     * @return true
+     */
+    public function sendFeedback($object = null, $feedback = "")
+    {
+        return true;
+    }
+
+    public function setFieldMapping($fieldMapping)
+    {
+    }
 }
